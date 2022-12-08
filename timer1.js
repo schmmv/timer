ab5f70a87bf8538205074c1cf60c5bc44d9be194
Michele Schulz <michelecaleb.schulz@gmail.com>
2022-12-08 23:22:00 +0000

    Refactored to wrap logic into a function.

--- timer1.js
+++ timer1.js
@@ -1,13 +1,17 @@
 //An 'alarm clock/timer' that beeps after a specified amount of time has passed.
 //User specifies an unlimited number of alarm times using the command line
 
-const seconds = process.argv.slice(2).sort((a, b) => a - b);
-
-for (const item of seconds) {
-  if (seconds.length === 0) return 0; //does nothing if no arguments/numbers are provided
+const timer = function (times) {
+for (const item of times) {
+  if (times.length === 0) return 0; //does nothing if no arguments/numbers are provided
   if (typeof(Number(item)) === 'number' && item >= 0) { //skips any negative numbers or arguments that are not numbers
     setTimeout(() => {
       process.stdout.write('\x07');
     }, item * 1000);
   }
 }
+};
+
+const seconds = process.argv.slice(2).sort((a, b) => a - b);
+
+timer(seconds);
